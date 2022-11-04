c36c886834e9f5a048a3d0bca87ec99a30f20708
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-11-04 16:35:11 +0100

    Search Insert Position

--- easy/35. Search Insert Position.cpp
+++ easy/35. Search Insert Position.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] >= target) return i;
+        int k = 0;
+        int n = nums.size();
+        for (int b = n/2; b > 0; b /= 2) {
+            while (k+b < n && nums[k+b] < target) k += b;
         }
-        return nums.size();
+        return nums[k] < target ? k+1 : 0;
     }
 };

d1a5df4905fa7822795254695c2ec231a4132e99
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-11-04 16:20:57 +0100

    Binary Search

--- easy/704. Binary Search.cpp
+++ easy/704. Binary Search.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        auto k = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        if (k < nums.size() && nums[k] == target) return k;
+        int k = 0;
+        int n = nums.size();
+        for (int b = n/2; b >= 1; b /= 2) {
+            while (k+b < n && nums[k+b] <= target) k += b;
+        }
+        if (nums[k] == target) return k;
         return -1;
     }
 };

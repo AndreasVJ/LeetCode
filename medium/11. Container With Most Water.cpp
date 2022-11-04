943f2b0f7cde0eec57aca56f94f9ac4e97247b29
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-11-04 21:30:08 +0100

    Container With Most Water

---
+++ medium/11. Container With Most Water.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int a = 0;
+        int b = height.size()-1;
+        int ans = 0;
+        while (a < b) {
+            ans = max(ans, (b-a)*min(height[a], height[b]));
+            if (height[a] < height[b]) a++;
+            else b--;
+        }
+        return ans;
+    }
+};

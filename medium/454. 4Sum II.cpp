9e38094a61678d0381264e0cbf2e29fb213fb399
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-11-04 22:21:05 +0100

    4Sum II

---
+++ medium/454. 4Sum II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        map<int, int> m;
+        int ans = 0;
+        for (int n1 : nums1) {
+            for (int n2 : nums2) {
+                m[n1+n2]++;
+            }
+        }
+        for (int n3 : nums3) {
+            for (int n4 : nums4) {
+                ans += m[-n3-n4];
+            }
+        }
+        return ans;
+    }
+};

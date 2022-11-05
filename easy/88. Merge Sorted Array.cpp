77f2c749b68a76fd126a8e1fee006b50a0d52c0c
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-11-05 12:11:19 +0100

    Merge Sorted Array

--- easy/88. Merge Sorted Array.cpp
+++ easy/88. Merge Sorted Array.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = m; i < m+n; i++) {
-            nums1[i] = nums2[i-m];
+        while (n) {
+            if (m && nums1[m-1] > nums2[n-1]) {
+                nums1[m+n-1] = nums1[m-1];
+                m--;
+            }
+            else {
+                nums1[m+n-1] = nums2[n-1];
+                n--;
+            }
         }
-        sort(nums1.begin(), nums1.end());
     }
 };

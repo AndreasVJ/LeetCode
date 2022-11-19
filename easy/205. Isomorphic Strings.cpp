bb4ff4bc77490f26fc9f10db65d90578062b8983
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-11-19 11:05:59 +0100

    Isomorphic Strings

--- easy/205. Isomorphic Strings.cpp
+++ easy/205. Isomorphic Strings.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char, char> m1, m2;
-        for (int i = 0; i < s.size(); i++) {
-            if (m1.find(s[i]) == m1.end()) m1[s[i]] = t[i];
-            else if (m1[s[i]] != t[i]) return 0;
-            if (m2.find(t[i]) == m2.end()) m2[t[i]] = s[i];
-            else if (m2[t[i]] != s[i]) return 0;
+        unordered_map<char, char> m;
+        unordered_set<char> seen;
+        for (int i = 0; i < t.size(); i++) {
+            if (m.find(t[i]) != m.end()) {
+                if (m[t[i]] != s[i]) return 0;
+            }
+            else {
+                if (seen.count(s[i])) return 0;
+                m[t[i]] = s[i];
+                seen.insert(s[i]);
+            }
         }
         return 1;
     }

d78cd5e6062b5e4d29c2e838ab1d623b2773696b
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-11-19 10:50:06 +0100

    Find Pivot Index

--- easy/724. Find Pivot Index.cpp
+++ easy/724. Find Pivot Index.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int sum = 0;
-        for (int i : nums) sum += i;
-        if (sum-nums[0] == 0) return 0;
         for (int i = 1; i < nums.size(); i++) {
-            if (sum-2*nums[i-1] == nums[i]) return i;
             nums[i] += nums[i-1];
         }
+        
+        if (nums.back()-nums[0] == 0) return 0;
+        
+        for (int i = 1; i < nums.size()-1; i++) {
+            if (nums.back()-nums[i] == nums[i-1]) return i;
+        }
+        
+        if (nums[nums.size()-2] == 0) return nums.size()-1;
+        
         return -1;
     }
 };

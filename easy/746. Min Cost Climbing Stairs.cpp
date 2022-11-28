5dbd537efd98e57d1f6886f5ffe466e2e86653b2
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-11-28 13:37:08 +0100

    Min Cost Climbing Stairs

--- easy/746. Min Cost Climbing Stairs.cpp
+++ easy/746. Min Cost Climbing Stairs.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        for (int i = 2; i < n; i++) {
-            cost[i] = min(cost[i-1] + cost[i], cost[i-2] + cost[i]);
+        for (int i = 2; i < cost.size(); i++) {
+            cost[i] = min(cost[i-1], cost[i-2]) + cost[i];
         }
-        return min(cost[n-1], cost[n-2]);
+        return min(cost.back(), cost[cost.size()-2]);
     }
 };

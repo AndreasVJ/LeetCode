b4442d63e7360ed578e5b32df5a1ef856ed32d5c
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-11-28 14:03:21 +0100

    Remove Element

--- easy/27. Remove Element.cpp
+++ easy/27. Remove Element.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int n = nums.size();
+        int n = 0;
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == val) {
-                nums.erase(nums.begin()+i);
-                i--;
-                n--;
+            if (n && nums[i] != val) {
+                nums[i-n] = nums[i];
             }
+            else if (nums[i] == val) n++;
         }
-        return n;
+        nums.resize(nums.size()-n);
+        return nums.size();
     }
 };

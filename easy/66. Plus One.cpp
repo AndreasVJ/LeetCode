335179b4869f543dd351581feb77ae5e88b5ab00
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-12-04 17:44:46 +0100

    Plus One

--- easy/66. Plus One.cpp
+++ easy/66. Plus One.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        for (int i = digits.size()-1; i >= 0; i--) {
-            if (digits[i] == 9) {
-                if (i == 0) {
-                    digits[0] = 1;
-                    digits.push_back(0);
-                }
-                else {
-                    digits[i] = 0;
-                }
-            }
-            else {
+        
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
+            digits[i] = 0;
         }
+        
+        digits[0] = 1;
+        digits.push_back(0);
+        
         return digits;
     }
 };

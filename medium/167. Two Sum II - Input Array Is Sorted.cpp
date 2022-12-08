ae7dc144751beb045c60c8c61ce0f4b393510252
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-12-08 20:44:20 +0100

    Two Sum II - Input Array Is Sorted

--- medium/167. Two Sum II - Input Array Is Sorted.cpp
+++ medium/167. Two Sum II - Input Array Is Sorted.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+
         int a = 0;
         int b = numbers.size()-1;
+
         while (numbers[a]+numbers[b] != target) {
-            if (numbers[a]+numbers[b] > target) b--;
-            else a++;
+            for (int k = (b - a)/2; k > 0; k /= 2) {
+                while (b - k > a && numbers[a] + numbers[b - k] > target) b -= k;
+                if (numbers[a] + numbers[b] > target) b--;
+                while (a < b - k && numbers[a + k] + numbers[b] < target) a += k;
+                if (numbers[a] + numbers[b] < target) a++;
+            }
         }
         return {a+1, b+1};
     }

a33f1cfa075b077a2885d9d9dcb89424ceb1572d
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-12-08 21:37:14 +0100

    Search a 2D Matrix

--- medium/74. Search a 2D Matrix.cpp
+++ medium/74. Search a 2D Matrix.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int h = matrix.size();
-        int l = matrix[0].size();
+
         int x = 0;
         int y = 0;
-        for (int b = h/2; b > 0; b /= 2) {
-            while (y+b < h && matrix[y+b][0] <= target) y += b;
+
+        for (int b = matrix.size()/2; b > 0; b /= 2) {
+            while (y + b < matrix.size() && matrix[y + b][0] <= target) y += b;
         }
-        for (int b = l/2; b > 0; b /= 2) {
-            while (x+b < l && matrix[y][x+b] <= target) x += b;
+
+        for (int b = matrix[0].size()/2; b > 0; b /= 2) {
+            while (x + b < matrix[0].size() && matrix[y][x + b] <= target) x += b;
         }
+
         return matrix[y][x] == target;
     }
 };

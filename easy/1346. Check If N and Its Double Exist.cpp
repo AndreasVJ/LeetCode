9c63611f4e36af7099174895999e802f977665f9
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-12-11 17:34:08 +0100

    Check If N and Its Double Exist

--- easy/1346. Check If N and Its Double Exist.cpp
+++ easy/1346. Check If N and Its Double Exist.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        for (int i = 0; i < arr.size(); i++) {
-            for (int n = 0; n < arr.size(); n++) {
-                if (i != n && arr[i] == 2 * arr[n]) return 1;
+        
+        sort(arr.begin(), arr.end());
+
+        for (int k, i = 0; i < arr.size(); i++) {
+            k = i;
+            if (arr[i] > 0) {
+                for (int b = (arr.size() - i)/2; b > 0; b /= 2) {
+                    while (k < arr.size() - b && arr[k + b] <= 2*arr[i]) k += b;
+                }
             }
+            else {
+                for (int b = i; b > 0; b /= 2) {
+                    while (k - b >= 0 && arr[k - b] >= 2*arr[i]) k -= b;
+                }
+            }
+            if (i != k && arr[k] == 2*arr[i]) return 1;
         }
+        
         return 0;
     }
 };

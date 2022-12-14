77421d0f6d7a2b1978003a76a4baf3fbce88fe06
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-12-14 22:33:58 +0100

    Pascal's Triangle

--- easy/118. Pascal's Triangle.cpp
+++ easy/118. Pascal's Triangle.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> out;
-        for (int i = 0; i < numRows; i++) {
-            if (i == 0) {
-                out.push_back({1});
+        
+        vector<vector<int>> ans;
+        
+        ans.push_back({1});
+        
+        for (int i = 1; i < numRows; i++) {
+            ans.push_back({1});
+            for (int n = 0; n < i-1; n++) {
+                ans[i].push_back(ans[i-1][n]+ans[i-1][n+1]);
             }
-            else {
-                vector<int> row = {1};
-                for (int n = 0; n < i-1; n++) {
-                    row.push_back(out[i-1][n]+out[i-1][n+1]);
-                }
-                row.push_back(1);
-                out.push_back(row);
-            }
+            ans[i].push_back(1);
         }
-        return out;
+
+        return ans;
     }
 };

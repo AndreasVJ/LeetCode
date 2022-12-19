6624544103ffae57063c1c9999c2cf61b3951296
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-12-19 20:58:15 +0100

    Largest Component Size by Common Factor

--- hard/952. Largest Component Size by Common Factor.cpp
+++ hard/952. Largest Component Size by Common Factor.cpp
@@ -39,17 +39,6 @@ class UnionFind {
 
 class Solution {
 public:
-
-    int gcd(int a, int b) {
-        if (b > a) {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-        if (a % b == 0) return b;
-        else return gcd(b, a%b);
-    }
-
     int largestComponentSize(vector<int>& nums) {
 
         int maxVal = 0;

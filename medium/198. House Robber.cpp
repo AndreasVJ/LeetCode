612b0dadf44c8288332af352384ab1276a2204dc
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-12-19 18:26:47 +0100

    House Robber

--- medium/198. House Robber.cpp
+++ medium/198. House Robber.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        
         if (nums.size() == 1) return nums[0];
+        
         nums[1] = max(nums[0], nums[1]);
+        
         for (int i = 2; i < nums.size(); i++) {
-            if (nums[i-1] > nums[i] + nums[i-2]) nums[i] = nums[i-1];
-            else nums[i] += nums[i-2];
+            nums[i] = max(nums[i - 2] + nums[i], nums[i - 1]);
         }
-        return max(nums.back(), nums[nums.size()-2]);
+        
+        return nums.back();
     }
 };

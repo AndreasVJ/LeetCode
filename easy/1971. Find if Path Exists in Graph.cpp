b1d611a1713fe9bc67cd9f95ff18a12b9dcf6950
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-12-26 14:15:59 +0100

    Find if Path Exists in Graph

---
+++ easy/1971. Find if Path Exists in Graph.cpp
@@ -0,0 +1,58 @@
+class UnionFind {
+    public:
+        UnionFind(int size) : root(size), rank(size) {
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        int find(int x) {
+            if (root[x] == x) {
+                return x;
+            }
+            return root[x] = find(root[x]); 
+        }
+
+        void unionSet(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    root[rootY] = rootX;
+                }
+                else if (rank[rootX] < rank[rootY]) {
+                    root[rootX] = rootY;
+                }
+                else {
+                    root[rootY] = rootX;
+                    rank[rootX]++;
+                }
+            }
+        }
+
+        bool connected(int x, int y) {
+            return find(x) == find(y);
+        }
+    
+    private:
+        std::vector<int> root;
+        std::vector<int> rank;
+};
+
+
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        
+        UnionFind uf(n);
+        
+        for (vector<int> edge : edges) {
+            uf.unionSet(edge[0], edge[1]);
+        }
+        
+        return uf.connected(source, destination);
+        
+    }
+};

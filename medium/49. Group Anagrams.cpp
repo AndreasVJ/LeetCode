bc2ff504f82aea0a47150a3102e1d7c885575d7e
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-12-31 16:45:09 +0100

    Group Anagrams

--- medium/49. Group Anagrams.cpp
+++ medium/49. Group Anagrams.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> m;
-        for (int i = 0; i < strs.size(); i++) {
-            string s = strs[i];
-            sort(strs[i].begin(), strs[i].end());
-            m[strs[i]].push_back(s);
-        }
+        
         vector<vector<string>> ans;
-        for (auto i : m) {
-            ans.push_back(i.second);
+
+        unordered_map<string, vector<int>> m;
+
+        for (int i = 0; i < strs.size(); i++) {
+
+            string temp = strs[i];
+
+            sort(temp.begin(), temp.end());
+            m[temp].push_back(i);
         }
+
+        for (auto const& [key, indexes] : m) {
+            ans.push_back({});
+            for (int i : indexes) {
+                ans.back().push_back(strs[i]);
+            }
+        }
+
         return ans;
     }
 };

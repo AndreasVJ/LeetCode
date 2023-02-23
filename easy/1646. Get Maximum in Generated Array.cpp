6d3d45beb5240c5ca9e6f591122b9c0fc84c833e
Andreas Jonsterhaug <andreas.vj@icloud.com>
2023-02-23 19:07:46 +0100

    Get Maximum in Generated Array

---
+++ easy/1646. Get Maximum in Generated Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+
+        if (n < 2) return n;
+
+        vector<int> arr(n+1, -1);
+        arr[0] = 0;
+        arr[1] = 1;
+        
+        int maximum = 1;
+
+        for (int i = 2; i < n+1; i++) {
+            if (i%2 == 0) {
+                arr[i] = arr[i/2];
+            }
+            else {
+                arr[i] = arr[i/2] + arr[i/2 + 1];
+            }
+            maximum = max(maximum, arr[i]);
+        }
+
+        return maximum;
+    }
+};

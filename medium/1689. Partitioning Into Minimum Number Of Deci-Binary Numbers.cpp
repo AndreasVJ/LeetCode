3193f1f66311a152f3baebef7f0224ac98a54221
Andreas Jonsterhaug <andreas.vj@icloud.com>
2023-06-22 15:34:12 +0200

    Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ medium/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        
+        char ans = '0';
+
+        for (char c : n) {
+            ans = max(ans, c);
+        }
+
+        return ans - '0';
+    }
+};

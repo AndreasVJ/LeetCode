b208248b749828c25168ad5133d7b849d2ceb7a8
AndreasVJ <andreas.vj@icloud.com>
2023-12-05 12:12:10 +0100

    Count of Matches in Tournament

---
+++ algorithms/easy/1688. Count of Matches in Tournament.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        if (n==1) return 0;
+        if (n%2) return numberOfMatches(n/2 + 1) + n/2;
+        return numberOfMatches(n/2) + n/2;
+    }
+};
